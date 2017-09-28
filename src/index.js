f5792d0a14d1da7273e82ea0e4ed3be6c6afb1c8
stalintouch <stalintouch1983@gmail.com>
2017-09-28 11:04:40 -0400

    added working board and seeded

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+class Box extends React.Component {
+
+  selectBox = () => {
+    this.props.selectBox(this.props.row, this.props.col)
+  }
+  render(){
+    return(
+      <div className={this.props.boxClass} id={this.props.id} onClick={this.selectBox}>
+        
+      </div>
+    )
+  }
+}
+
 class Grid extends React.Component{
   render(){
-    const width= this.props.cols * 14;
+    const width= (this.props.cols * 16) + 1;
     var rowsArr = [];
     var boxClass="";
     for (var i =0; i< this.props.rows; i++){
@@ -26,7 +41,7 @@ class Grid extends React.Component{
 
     return(
       <div className="grid" style={{width:width}}>
-        {{rowsArr}}
+        {rowsArr}
       </div>
     )
   }
@@ -43,6 +58,33 @@ class Main extends React.Component{
       gridFull: Array(this.rows).fill().map(()=> Array(this.cols).fill(false))
     }
   }
+
+  selectBox =(row, col) => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    gridCopy[row][col] = !gridCopy[row][col];
+    this.setState({
+      gridFull: gridCopy
+    })
+  }
+
+  seed =() =>{
+    let gridCopy = arrayClone(this.state.gridFull);
+    for (var i =0; i< this.rows; i++){
+      for (var j =0; j< this.cols; j++){
+        if(Math.floor(Math.random() * 4) === 1){
+          gridCopy[i][j] = true
+        }
+      }
+    }
+    this.setState({
+      gridFull: gridCopy
+    })
+  }
+
+  componentDidMount() {
+    this.seed();  
+  }
+
   render(){
     return(
       <div>
@@ -57,6 +99,10 @@ class Main extends React.Component{
       </div>
     )
   }
+}
+
+function arrayClone(arr){
+  return JSON.parse(JSON.stringify(arr));
 }
 
 ReactDOM.render(<Main />, document.getElementById('root'));
